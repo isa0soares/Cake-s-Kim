263a32917e44cb557d96bd605bfecb0712b64e5b
isa0soares <148557925+isa0soares@users.noreply.github.com>
2023-10-26 11:36:14 -0300

    Add files via upload

---
+++ Sistema Isa/index.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cake's KimJ</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+<body class="bg-success-subtle">
+
+</body>
+<div class="container text-center">
+        <div class="mt-5 mb-3">
+            <img src="Recursos\foto.png" alt="" width="300px">
+        </div>
+        <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Usuário</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">Seu e-mail</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">senha</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+    <div id="exampleInputPassword1" class="form-text">Entre e seja feliz</div>
+
+    <form>
